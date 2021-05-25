ae222fa23231bfb1d605f155a5346edcc615c868
Stephan Schmitz <stephan.schmitz@sunzinet.com>
2021-05-25 16:12:38 +0200

    Initial release

---
+++ precache-manifest.5484315c65ddf74ff4dcdcbbdf6caeba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d641a0840013aa0892cc4387c5cc303e",
+    "url": "/img/ic_test_result_illustration_negative.d641a084.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2e5bd4943bd43de913ad",
+    "url": "/js/chunk-vendors.57236df2.js"
+  },
+  {
+    "revision": "6487bf23e9d7cf140f7b",
+    "url": "/js/app.77906611.js"
+  },
+  {
+    "revision": "287ced8241cb1e102941",
+    "url": "/js/about.afc9756c.js"
+  },
+  {
+    "revision": "d7f836e2a5cf3a89099f8487fbf75216",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ccdd17ba372350fee480f29118d45a07",
+    "url": "/img/ic_main_illustration_warnende_personen.ccdd17ba.png"
+  },
+  {
+    "revision": "d5021b35b5d28497620879a685d8ef0c",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "2e5bd4943bd43de913ad",
+    "url": "/css/chunk-vendors.922cb2e0.css"
+  },
+  {
+    "revision": "6487bf23e9d7cf140f7b",
+    "url": "/css/app.f829867f.css"
+  }
+];
