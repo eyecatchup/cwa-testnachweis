5b94488c7cdeec37f565ca1dd2cce727c5aecb5a
Stephan Schmitz <stephan.schmitz@sunzinet.com>
2021-05-26 13:42:50 +0200

    style updates

---
+++ precache-manifest.3baf7be1c81ab2d3b5baeea8552ff46e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e68f6bee37893d4b2d9cf76ba1b05846",
+    "url": "/cwa-testnachweis/img/ic_main_about.e68f6bee.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cwa-testnachweis/robots.txt"
+  },
+  {
+    "revision": "db19248198b46e3c2649",
+    "url": "/cwa-testnachweis/js/chunk-vendors.94021960.js"
+  },
+  {
+    "revision": "53cb16d3a7d9d8ba5740",
+    "url": "/cwa-testnachweis/js/app.2cc7a7c6.js"
+  },
+  {
+    "revision": "2867e7301df27e3dc82cf2829dc62752",
+    "url": "/cwa-testnachweis/index.html"
+  },
+  {
+    "revision": "b7d9ad343bfb595b6fd86b9628fcd09e",
+    "url": "/cwa-testnachweis/img/ic_test_result_illustration_negative.b7d9ad34.png"
+  },
+  {
+    "revision": "c5df3581afe412564fbdee576de8a311",
+    "url": "/cwa-testnachweis/img/ic_settings_background_priority_enabled.c5df3581.png"
+  },
+  {
+    "revision": "47c431d8d19e3665073f5d3aedb8c6fb",
+    "url": "/cwa-testnachweis/img/ic_main_illustration_untested.47c431d8.png"
+  },
+  {
+    "revision": "d5021b35b5d28497620879a685d8ef0c",
+    "url": "/cwa-testnachweis/favicon.png"
+  },
+  {
+    "revision": "db19248198b46e3c2649",
+    "url": "/cwa-testnachweis/css/chunk-vendors.08e26768.css"
+  },
+  {
+    "revision": "53cb16d3a7d9d8ba5740",
+    "url": "/cwa-testnachweis/css/app.24235e8d.css"
+  }
+];
